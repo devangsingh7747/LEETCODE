bf6ee6d638c11eef7dc6f91dbcca3dbad4418a27
Devang Singh <100057613+devangsingh7747@users.noreply.github.com>
2025-08-20 09:27:37 +0530

    Add files via upload

---
+++ 1277. Count Square Submatrices with All Ones.java
@@ -0,0 +1,84 @@
+/*
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+
+Example 1:
+
+Input: matrix =
+[
+    [0,1,1,1],
+    [1,1,1,1],
+    [0,1,1,1]
+]
+Output: 15
+Explanation:
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+
+Example 2:
+
+Input: matrix =
+[
+    [1,0,1],
+    [1,1,0],
+    [1,1,0]
+]
+Output: 7
+Explanation:
+There are 6 squares of side 1.
+There is 1 square of side 2.
+Total number of squares = 6 + 1 = 7.
+
+
+Constraints:
+
+1 <= arr.length <= 300
+1 <= arr[0].length <= 300
+0 <= arr[i][j] <= 1
+*/
+
+
+
+
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int [][] dp = new int[m + 1][n + 1];
+        int sqMatrix = 0;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(matrix[i - 1][j - 1] == 1) {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+                    sqMatrix += dp[i][j];
+                }
+            }
+        }
+
+        return sqMatrix;
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[][] matrix1 = {
+            {0, 1, 1, 1},
+            {1, 1, 1, 1},
+            {0, 1, 1, 1}
+        };
+
+        int[][] matrix2 = {
+            {1, 0, 1},
+            {1, 1, 0},
+            {1, 1, 0}
+        };
+
+        System.out.println("Count of square submatrices in matrix1: " + sol.countSquares(matrix1));
+        System.out.println("Count of square submatrices in matrix2: " + sol.countSquares(matrix2));
+    }
+}
